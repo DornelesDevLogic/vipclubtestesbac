712634521caedd75d2d867e89bc36748a06494a5
DornelesDevLogic <dorneles@logicbox.com.br>
2025-10-28 09:07:45 -0300

    Sua mensagem descritiva aqui

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { startQueueProcess } from "./queues";
 import { TransferTicketQueue } from "./wbotTransferTicketQueue";
 import cron from "node-cron";
 
-
+//Teste git 28102025
 const server = app.listen(process.env.PORT, async () => {
   const companies = await Company.findAll();
   const allPromises: any[] = [];
